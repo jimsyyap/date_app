9b25c33e74f9f3f77a0255989911d679d099c503
jimmy desu <jimsyyap@gmail.com>
2024-08-08 09:46:44 +1000

    added updates to payment system

--- README.md
+++ README.md
@@ -7,11 +7,14 @@ golang back-end; react front-end
 
 - [ ] api recipes available for users
 - [ ] api for admin?
-- [ ] set up db (postgres)
+- [ ] set up db (mongodb)
 - [ ] matching algorithm using ML/AI
+- [ ] redifine Recipe struct (backend/cmd/api) to include details for user admin & matching
 
 ### changelog
 
+- looking into mongodb over postgres. See terms&conditions for details.
+- rm backend/cmd/api
 - changed backend/cmd/api to user-recipes, not movies
 
 ### explorations
---
+++ backend/recipes/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"context"
+	"github.com/gin-gonic/gin"
+	"github.com/go-redis/redis"
+	handlers "github.com/jimsyyap/date_app/handlers"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/mongo/readpref"
+	"log"
+	"os"
+)
+
+var recipesHandler *handlers.recipeHandler
+
+func init() {
+	ctx := context.Background()
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI(os.Getenv("MONGO_URI")))
+	if err = client.Ping(context.TODO(), readpref.Primary()); err != nil {
+		log.Fatal(err)
+	}
+	log.Println("Connected to MongoDB")
+	collection := client.Database("date_app").Collection("recipes")
+
+	redisClient := redis.NewClient(&redis.Options{
+		Addr:     "localhost:6379",
+		Password: "",
+		DB:       0,
+	})
+
+	status := redisClient.Ping()
+	log.Println(status)
+
+	recipesHandler = handlers.NewRecipeHandler(collection, redisClient)
+}
+
+func main() {
+	router := gin.Default()
+	router.POST("/recipes", recipesHandler.NewRecipeHandler)
+	router.GET("/recipes", recipesHandler.ListRecipesHandler)
+	router.PUT("/recipes/:id", recipesHandler.UpdateRecipeHandler)
+	router.DELETE("/recipes/:id", recipesHandler.DeleteRecipeHandler)
+	router.GET("/recipes/:id", recipesHandler.GetOneRecipeHandler)
+	/*
+		router.GET("/recipes/search", SearchRecipesHandler)*/
+	router.Run()
+}

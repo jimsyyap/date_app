0c349dec6494763a81e9c97cf252cd7cdb5c185a
jimmy desu <jimsyyap@gmail.com>
2024-08-09 16:12:18 +1000

    updated frontend react

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,17 +1,41 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import AppHeader from './AppHeader';
+import AppContent from './AppContent';
+import AppFooterFunctionalComponent from './AppFooterFunctionalComponent';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+class App extends Component {
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+    constructor(props) {
+        super(props);
+        this.handlePostChange = this.handlePostChange.bind(this);
+        this.state = { posts: [] };
+    }
+
+    handlePostChange(posts) {
+        this.setState({ posts: posts });
+    }
+
+    render() {
+        const myProps = {
+            title: "My Cool App!",
+            subject: "My subject",
+            favourite_color: "red",
+        }
+
+        return (
+            <div className="app">
+                <AppHeader {...myProps} posts={this.state.posts} handlePostChange={this.handlePostChange} />
+                <AppContent handlePostChange={this.handlePostChange} />
+                {/* <AppFooter /> */}
+                <AppFooterFunctionalComponent myProperty={"Hello, world"} />
+            </div>
+        );
+    }
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));

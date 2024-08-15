e07fca96b914b7892194975063f67d53e558969d
jimmy desu <jimsyyap@gmail.com>
2024-08-15 18:45:55 +1000

    update

---
+++ backend/paymntsystm/cmd/api/api.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"time"
+)
+
+const version = "1.0.0"
+
+type config struct {
+	port int
+	env  string
+	db   struct {
+		dsn string
+	}
+	stripe struct {
+		secret string
+		key    string
+	}
+}
+
+type appliction struct {
+	config   config
+	infoLog  *log.Logger
+	errorLog *log.Logger
+	version  string
+}
